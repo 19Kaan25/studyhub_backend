ed062309e7f0ada37cb9dbf0a73f56cc2c89f009
19Kaan25 <kaan.koeten@gmx.de>
2026-06-03 22:53:55 +0200

    Milestone 4 In ResourceController CrossOrigin Annotation eingefügt

--- src/main/java/htw/webtech/studyhub/resource/ResourceController.java
+++ src/main/java/htw/webtech/studyhub/resource/ResourceController.java
@@ -1,11 +1,9 @@
 package htw.webtech.studyhub.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class ResourceController {
 
@@ -17,6 +15,7 @@ public class ResourceController {
         return service.save(resource);
     }
 
+    @GetMapping("/resources/{id}")
     public Resource getResource(@PathVariable String id) {
         Long resourceId = Long.parseLong(id);
         return service.get(resourceId);
